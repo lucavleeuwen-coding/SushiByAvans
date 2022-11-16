6c9e5e3e52b94b253151b2235df83d0b4717bc64
lucavleeuwen-coding <62882292+lucavleeuwen-coding@users.noreply.github.com>
2022-11-16 22:31:54 +0100

    Add files via upload

---
+++ SushiByAvans/src/main/java/com/example/SushiByAvans/SushiByAvansApplication.java
@@ -0,0 +1,13 @@
+package com.example.SushiByAvans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiByAvansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiByAvansApplication.class, args);
+	}
+
+}
---
+++ SushiByAvans/src/test/java/com/example/SushiByAvans/SushiByAvansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SushiByAvans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiByAvansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
